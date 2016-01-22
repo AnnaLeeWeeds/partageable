d763bc233f48130c7dac60814565987aabc6fb3e
AnnaLeeWeeds <anna.kodjo@gmail.com>
2016-01-22 14:12:12 +0100

    Composant 3 appelle Composant1

---
+++ Composant3/Composant3.cpp
@@ -0,0 +1,17 @@
+#include "Composant3.h"
+
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
+COMPOSANT3_INTERFACE int  composant3(int p1, int p2)
+{
+
+	return p1 + p2;
+}
+
+COMPOSANT3_INTERFACE char * getComposant3Version()
+{
+	return "Composant 3 version ";
+}
