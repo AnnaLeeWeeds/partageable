78b37e81afa9c0f52e8b702dc1f120eda231fa12
AnnaLeeWeeds <anna.kodjo@gmail.com>
2016-01-22 19:07:46 +0100

    Fin Exercie Composant 4 substituer a Composant 1

---
+++ Composant4/Composant4Version.h
@@ -0,0 +1,16 @@
+#define ComposantVersion_5 1
+#define ComposantVersion_6 2
+#define ComposantVersion_7 0
+#define ComposantVersion_8 0
+
+/////////// apres cette ligne: ne pas modifier ///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+#define COMPOSANT_VERSION ComposantVersion_5,ComposantVersion_6,ComposantVersion_7,ComposantVersion_8
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define VERSION_TEXT(_version,_build,_interface,_fonction) \
+	TOSTRING(_version) TOSTRING(.) TOSTRING(_build) TOSTRING(.) TOSTRING(_interface) STRINGIFY(.) TOSTRING(_fonction)
+
+#define COMPOSANT_VERSION_STR VERSION_TEXT(ComposantVersion_5,ComposantVersion_6,ComposantVersion_7,ComposantVersion_8)
