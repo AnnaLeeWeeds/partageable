0bada9fdee07da8159816db65c5a54076d2d0a15
AnnaLeeWeeds <anna.kodjo@gmail.com>
2016-01-22 20:19:40 +0100

    Integration des headers

--- Composant4/Composant4.cpp
+++ Composant4/Composant4.cpp
@@ -4,26 +4,13 @@
 #include "../Composant3/Composant3.h"
 
 // declaration des fonctions internes
-int addition_interne(int a1, int a2);
 
-
-
-int composant1(int p1, int p2)
+int composant4(int p1, int p2)
 {
 	return composant3(p1, p2);
 }
-
-int addition_interne(int a1, int a2)
-{
-	return a1 + a2;
-}
-
 char * getComposant4Version()
 {
 
 	return "Composant 4 version " COMPOSANT_VERSION_STR;
-}
-int composant4(int p1, int p2)
-{
-	return composant3(p1, p2);
 }
